5845c77418a8f1b7d9346607bf4fa74cea8b81f1
Levi Abellar <85912450+leviabellar@users.noreply.github.com>
2021-06-21 18:04:22 +0800

    Using Filesystem Modules

--- app.js
+++ app.js
@@ -15,13 +15,22 @@
 
 //const path = require('path');
 const os = require('os');
+const fs = require('fs');
 
-var totalMemory = os.totalmem();
-var freeMemory = os.freemem();
+// var totalMemory = os.totalmem();
+// var freeMemory = os.freemem();
 
-console.log(`Total Memory: ${totalMemory}`);
-console.log(`Free Memory: ${freeMemory}`);
+// console.log(`Total Memory: ${totalMemory}`);
+// console.log(`Free Memory: ${freeMemory}`);
 
 //var pathObj = path.parse(__filename);
 
 //console.log(pathObj);
+
+const files = fs.readdirSync('./');
+console.log(files);
+
+fs.readdir('$', function(err, files) {
+    if (err) console.log('Error', err);
+    else console.log('Result', files);
+});
